c064496dac83d8f3393aa7f080aa845e44ca65ee
dongkykonglin <isaaclin54@gmail.com>
2023-06-04 00:26:26 -0700

    create navbar components

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 // import styles from './page.module.css'
 
 export default function Home() {
-  return <h1 className="text-red-500">Home</h1>;
+  return <h1 className="text-red-500"></h1>;
 }
