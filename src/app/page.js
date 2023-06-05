aa344aab9373669650b1743c3c252a5c21e4f9eb
dongkykonglin <isaaclin54@gmail.com>
2023-06-04 21:01:59 -0700

    API set up

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,24 @@
-import Image from "next/image";
-// import styles from './page.module.css'
+const API_KEY = process.env.API_KEY;
+import Results from "./components/Results";
+const key = "881c4d4508497518c91eba5e7ab50d34";
 
-export default function Home() {
-  return <h1 className="text-red-500"></h1>;
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${key}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data from server");
+  }
+  const data = await res.json();
+  const results = data.results;
+  console.log(results);
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
